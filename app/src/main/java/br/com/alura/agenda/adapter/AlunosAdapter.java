6da9624832dae19ae9373473d707e7b6d0f669ca
Fabio <fabioalpl@gmail.com>
2018-03-09 18:05:10 -0300

    Parte 2 - Aula 3 - Melhorando o visual da lista de alunos: Melhorias no layout.

--- app/src/main/java/br/com/alura/agenda/adapter/AlunosAdapter.java
+++ app/src/main/java/br/com/alura/agenda/adapter/AlunosAdapter.java
@@ -1,9 +1,13 @@
 package br.com.alura.agenda.adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -11,6 +15,7 @@ import org.w3c.dom.Text;
 import java.util.List;
 
 import br.com.alura.agenda.ListaAlunosActivity;
+import br.com.alura.agenda.R;
 import br.com.alura.agenda.modelo.Aluno;
 
 /**
@@ -44,9 +49,28 @@ public class AlunosAdapter extends BaseAdapter{
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        TextView viewContext = new TextView(context);
+        //TextView viewContext = new TextView(context);
+        /*Aluno aluno = alunos.get(i);
+        viewContext.setText(alunos.toString());*/
         Aluno aluno = alunos.get(i);
-        viewContext.setText(alunos.toString());
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View viewContext = inflater.inflate(R.layout.list_item, viewGroup);
+
+        TextView campoNome = (TextView) viewContext.findViewById(R.id.item_nome);
+        campoNome.setText(aluno.getNome());
+
+        TextView campoTelefone = (TextView) viewContext.findViewById(R.id.item_telefone);
+        campoTelefone.setText(aluno.getTelefone());
+
+        ImageView campoFoto = (ImageView) viewContext.findViewById(R.id.item_foto);
+        String caminhoFoto = aluno.getCaminhoFoto();
+        if (caminhoFoto != null) {
+            Bitmap bitmap = BitmapFactory.decodeFile(caminhoFoto);
+            Bitmap bitmapReduzido = Bitmap.createScaledBitmap(bitmap, 100, 100, true);
+            campoFoto.setImageBitmap(bitmapReduzido);
+            campoFoto.setScaleType(ImageView.ScaleType.FIT_XY);
+        }
+
         return viewContext;
     }
 }

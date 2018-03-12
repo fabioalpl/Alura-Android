5ca5b41a3c70560b2d913a55bf326a9e11ac36d1
Fabio <fabioalpl@gmail.com>
2018-03-12 16:06:38 -0300

    Parte 2 - Aula 4 - Tratando eventos de sistema: Verificando se o SMS foi enviado por um aluno.

--- app/src/main/java/br/com/alura/agenda/receiver/SMSReceiver.java
+++ app/src/main/java/br/com/alura/agenda/receiver/SMSReceiver.java
@@ -3,7 +3,16 @@ package br.com.alura.agenda.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+
+import android.media.MediaPlayer;
+import android.os.Build;
+import android.telephony.SmsMessage;
 import android.widget.Toast;
+
+import br.com.alura.agenda.R;
+import br.com.alura.agenda.dao.AlunoDao;
+
+import static android.Manifest.permission_group.SMS;
 
 /**
  * Created by fabio on 11/03/2018.
@@ -12,6 +21,26 @@ import android.widget.Toast;
 public class SMSReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, "Chegou um SMS!", Toast.LENGTH_LONG).show();
+
+        Object [] pdus = (Object[]) intent.getSerializableExtra("pdus");
+        byte [] pdu = (byte[]) pdus[0];
+        String formato = (String) intent.getSerializableExtra("format");
+
+        SmsMessage sms = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            sms = SmsMessage.createFromPdu((byte[]) pdus[0], formato);
+        } else {
+            sms = SmsMessage.createFromPdu((byte[]) pdus[0]);
+        }
+        //SmsMessage sms = SmsMessage.createFromPdu(pdu, formato);
+
+        String telefone = sms.getDisplayOriginatingAddress();
+        AlunoDao dao = new AlunoDao(context);
+        if(dao.ehAluno(telefone)) {
+            Toast.makeText(context, "Chegou um SMS!", Toast.LENGTH_LONG).show();
+            MediaPlayer mp = MediaPlayer.create(context, R.raw.msg);
+            mp.start();
+        }
+        dao.close();
     }
 }

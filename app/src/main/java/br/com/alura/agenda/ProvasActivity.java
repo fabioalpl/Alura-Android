291bc69a7b1533f8cdd5aeb4bd98c3742497ba99
Fabio de Almeida <fabioalpl@gmail.com>
2018-04-01 20:49:41 -0300

    Android Parte 3 - Aula 2: Reaproveitando telas com Fragments.

---
+++ app/src/main/java/br/com/alura/agenda/ProvasActivity.java
@@ -0,0 +1,44 @@
+package br.com.alura.agenda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.Arrays;
+import java.util.List;
+
+import br.com.alura.agenda.modelo.Prova;
+
+public class ProvasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_provas);
+
+        List<String> topicosPort = Arrays.asList("Sujeito","Objeto direto", "Objeto Indireto");
+        Prova provaPortugues = new Prova("Portugues", "25/05/2018", topicosPort);
+
+        List<String> topicosMat = Arrays.asList("Equacoes de segundo grau","Trigonometria");
+        Prova provaMatematica = new Prova("Matematica","27/05/2018",topicosMat);
+
+        List<Prova> provas = Arrays.asList(provaPortugues, provaMatematica);
+
+        ArrayAdapter<Prova> adapter = new ArrayAdapter<Prova>(this, android.R.layout.simple_list_item_1, provas);
+
+        ListView lista = (ListView) findViewById(R.id.provas_lista);
+        lista.setAdapter(adapter);
+
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Prova prova = (Prova) adapterView.getItemAtPosition(i);
+                Toast.makeText(ProvasActivity.this, "Clicou na prova de: " + prova, Toast.LENGTH_SHORT ).show();
+            }
+        });
+    }
+}

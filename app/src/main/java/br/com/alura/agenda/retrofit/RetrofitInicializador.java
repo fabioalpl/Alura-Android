b9ce3e8e3c9720575f8f0d7abeb15e0de7acff57
Fabio de Almeida <fabioalpl@gmail.com>
2018-07-09 09:24:03 -0300

    Android Com WebService 1 - Realizando requisição.

--- app/src/main/java/br/com/alura/agenda/retrofit/RetrofitInicializador.java
+++ app/src/main/java/br/com/alura/agenda/retrofit/RetrofitInicializador.java
@@ -1,5 +1,6 @@
 package br.com.alura.agenda.retrofit;
 
+import br.com.alura.agenda.services.AlunoService;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 
@@ -8,8 +9,11 @@ public class RetrofitInicializador {
     private final Retrofit retrofit;
 
     public RetrofitInicializador(){
-        retrofit = new Retrofit.Builder().baseUrl("http://192.168.0.23:8080/api")
+        retrofit = new Retrofit.Builder().baseUrl("http://192.168.0.23:8080/api/")
                 .addConverterFactory(JacksonConverterFactory.create()).build();
     }
 
+    public AlunoService getAlunoService() {
+        return retrofit.create(AlunoService.class);
+    }
 }
